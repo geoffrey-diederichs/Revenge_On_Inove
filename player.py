86d44d65383fd77e28ba5784596021b3615c3043
alexandre <alexandre.bel@ynov.com>
2023-11-23 09:46:02 +0100

    new gitignore

---
+++ player.py
@@ -0,0 +1,7 @@
+class Player:
+    def __init__(self, x: int, y:int):
+        self.position_x = x
+        self.position_y = y
+        self.width = 16
+        self.height = 16
+        self.imgSrc = 'kfevkfa'
