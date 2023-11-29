5a2ee23aebcca61dfcd2ef65b4fea3f9722a1142
alexandre <alexandre.bel@ynov.com>
2023-11-29 16:31:16 +0100

    can now choose character

--- player.py
+++ player.py
@@ -9,14 +9,14 @@ class Player:
         self.frameY = 0
         self.frameRate = 0
         self.framesX = 8-1
-        self.framesY = 2
+        self.framesY = 2-1
         self.char = 1
         self.sprint = 0
         self.totalFrames = 60
 
     def animate(self, pressed: bool):
         if self.sprint == 0:
-            self.totalFrames = 60
+            self.totalFrames = 50
         elif self.sprint != 0:
             self.totalFrames = 30
             if self.frameRate > 30:
@@ -24,19 +24,22 @@ class Player:
 
         if self.char == 1:
             self.charFrame = 24
+            self.frame = 24
         elif self.char == 2:
             self.charFrame = 24*5
+            #self.frame = 24*5
         elif self.char == 3:
             self.charFrame = 24*9
+            self.frame = 24*9
 
         if pressed:
             if self.frameRate == self.totalFrames:
-                self.frameRate = 0
-                if self.frameY >= self.height*self.framesY:
-                    self.frameY = self.charFrame
+                if self.frameY < self.framesY*self.height+self.height+self.charFrame:
+                    self.frameY += self.height
                 else:
-                    self.frameY += self.height*2
+                    self.frameY = self.charFrame
+                self.frameRate = 0
             else:
-                self.frameRate += 1
+                self.frameRate+=1
         else:
             self.frameY = self.charFrame+self.height
