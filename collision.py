bb13022e54e38bd2de780515e540dd2c215d4497
Alexandre <bel.alexandre33@gmail.com>
2023-12-03 16:08:18 +0100

    can now start fiight

--- collision.py
+++ collision.py
@@ -1,11 +1,13 @@
 class Collision:
     allCollisions = []
-    def __init__(self, x, y):
+    def __init__(self, x, y, id = 126):
         self.x = x
         self.y = y
         self.size = 16
+        self.id = id
         #fixed are var that will stay at the original pos on the other hand, x and y will move to match the background movements
         self.fixedX = x
+        self.used = False
         self.fixedY = y
         Collision.allCollisions.append(self)
         
